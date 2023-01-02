c67397ed49a4210ec809258e8573546efc19cdae
sayali-2001 <sayalithakare47@gmail.com>
2023-01-02 22:44:08 +0530

    Added : Use Case 4

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,13 @@
 package NewPackage;
+import java.util.Random;
 import java.util.Scanner;
+
+import static NewPackage.TicTac.computerChoice;
+
 public class TicTacToe {
     static char[] board = new char[10];
+    public static Scanner scannerObject = new Scanner(System.in);
+    private static int playLocation;
     static char player,computer;
     public static Scanner scanner = new Scanner(System.in);
     static void createBoard(){
@@ -9,36 +15,48 @@ public class TicTacToe {
             board[index] = ' ';
         }
     }
-
-    static void getPlayerChoice() {
-        System.out.print("select x or o : ");
-        player = scanner.next().charAt(0);
-        if (player == 'x')
-            computer='o';
-        System.out.print("select X or O : ");
-        player = Character.toUpperCase(scanner.next().charAt(0));
-
-        if (player == 'X')
-            computer='O';
-        else
-            computer ='x';
-        computer ='X';
-        System.out.println("You have selected : " +player);
-        System.out.println("Computer's choice is : " +computer);
+    static void choice() {
+        int turn=(int) (Math.random() * 10) % 2;
+        if(turn == 0) {
+            System.out.println("user play first");
+        }else if(turn == 1) {
+            System.out.println("computer play first");
+        }
     }
+
+    static void computerChoice() {
+        int computerTurn=(int)(Math.random() * 10) % 10;
+        board[computerTurn]=computerChoice;
+        System.out.println("Computer choice is :: "+computerChoice);
+    }
+
 
     static void showBoard() {
         System.out.println("  " + board[1] + "  |  " + board[2]  + "   | " + board[3] + "  ");
         System.out.println("  " + board[4] + "  |  " + board[5]  + "   | " + board[6] + "  ");
         System.out.println("  " + board[7] + "  |  " + board[8]  + "   | " + board[9] + "  ");
     }
+    public static void userMove() {
+        System.out.println("Enter Location 1-9 to Make Move");
 
+         playLocation = scannerObject.nextInt();
+        if (playLocation < 10 && playLocation > 0) {
+            board[playLocation] = player;
+            showBoard();
+        } else {
+            System.out.println("Invalid Choice");
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
+
         createBoard();
-        getPlayerChoice();
+        choice();
+        computerChoice();
+//        getPlayerChoice();
         showBoard();
+        userMove();
     }
     }
 

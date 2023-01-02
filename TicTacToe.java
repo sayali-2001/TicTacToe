56b717f710cae432e6de5ad795da29e4bd6f71d6
sayali-2001 <sayalithakare47@gmail.com>
2023-01-02 22:22:49 +0530

    Added : Use Case 2

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,17 +2,36 @@ package NewPackage;
 import java.util.Scanner;
 public class TicTacToe {
     static char[] board = new char[10];
-
+    static char player,computer;
+    public static Scanner scanner = new Scanner(System.in);
     static void createBoard(){
         for(int index = 0 ; index<10 ; index++) {
             board[index] = ' ';
         }
     }
 
+    static void getPlayerChoice() {
+        System.out.print("select x or o : ");
+        player = scanner.next().charAt(0);
+        if (player == 'x')
+            computer='o';
+        System.out.print("select X or O : ");
+        player = Character.toUpperCase(scanner.next().charAt(0));
+
+        if (player == 'X')
+            computer='O';
+        else
+            computer ='x';
+        computer ='X';
+        System.out.println("You have selected : " +player);
+        System.out.println("Computer's choice is : " +computer);
+    }
+
     public static void main(String[] args) {
 
         System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
         createBoard();
+        getPlayerChoice();
     }
     }
 
